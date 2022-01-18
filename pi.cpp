034adf5298367f5443cd70ac9160bc8c6a78ab82
Zaid Parkar <zaids.parkar@gmail.com>
2022-01-18 15:00:45 +0530

    Add files via upload

---
+++ pi.cpp
@@ -0,0 +1,19 @@
+
+#include "bits/stdc++.h"
+using namespace std;
+
+void printValueOfPi(int N)
+{
+
+    double pi = 2 * acos(0.0);
+
+    printf("%.*lf\n", N, pi);
+}
+
+int main()
+{
+    int N = (145);
+
+    printValueOfPi(N);
+    return 0;
+}
